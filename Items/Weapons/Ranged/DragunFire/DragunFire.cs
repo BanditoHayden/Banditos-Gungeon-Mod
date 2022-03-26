2ae9705c695cada974276c6d31ef27e93e4d2df4
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-03-26 07:51:21 -0700

    Add files via upload

---
+++ Items/Weapons/Ranged/DragunFire/DragunFire.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GungeonMod;
+using GungeonMod.Items.Herbs;
+
+namespace GungeonMod.Items.Weapons.Ranged.DragunFire
+{
+	public class DragunFire : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dragunfire");
+			Tooltip.SetDefault("Roar\n");
+		}
+		public override void SetDefaults()
+		{
+			// Stats of the item
+			item.damage = 14;
+			item.useTime = 8;
+			item.useAnimation = 8;
+			item.knockBack = 0;
+			item.value = 10000;
+			item.ranged = true;
+			item.crit = 4;
+			// How the item works
+			item.autoReuse = true;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			item.shoot = mod.ProjectileType("DragunFireProj");
+			item.shootSpeed = 12.7f;
+			// Other
+			item.UseSound = SoundID.Item11;
+			item.rare = 3;
+			item.scale = 1.1f;
+		}
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 25f;
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+			return true;
+		}
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-2, 0);
+		}
+		
+
+	}
+}
---
+++ Items/Weapons/Ranged/DragunFire/DragunFireProj.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GungeonMod;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GungeonMod.Items.Weapons.Ranged.DragunFire
+{
+	public class DragunFireProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Fire");
+			Main.projFrames[projectile.type] = 10;
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 18;
+			projectile.height = 18;
+			projectile.penetrate = 1;
+			projectile.aiStyle = 1;
+			projectile.ranged = true;
+			projectile.friendly = true;
+			projectile.ignoreWater = false;
+			projectile.aiStyle = -1;
+			projectile.light = 0.5f;
+
+		}
+        public override void AI()
+        {
+			
+			if (++projectile.frameCounter >= 11)
+			{
+				projectile.frameCounter = 0;
+				if (++projectile.frame >= 10)
+				{
+					projectile.frame = 0;
+				}
+			}
+			if (projectile.penetrate <= 0)
+			{
+				projectile.Kill();
+			}
+			if (projectile.spriteDirection == -1)
+			{
+				projectile.rotation += MathHelper.Pi;
+			}
+
+		}
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			projectile.penetrate--;
+			Main.PlaySound(SoundLoader.customSoundType, -1, -1, mod.GetSoundSlot(SoundType.Custom, "Sounds/Custom/FlameImpact"));
+		
+			return false;
+		}
+		public override void Kill(int timeLeft)
+		{
+			// This code and the similar code above in OnTileCollide spawn dust from the tiles collided with. SoundID.Item10 is the bounce sound you hear.
+			Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+			Main.PlaySound(SoundID.Item10, projectile.position);
+		}
+	}
+}
+
+
