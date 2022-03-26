575f1d4572d836533b0ed58e8db74c61340106f8
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-03-26 16:19:44 -0700

    Add files via upload

---
+++ Items/Actives/PotionofGunFriendship/GunBuff.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GungeonMod;
+using GungeonMod.Items.Herbs;
+using System.Threading.Tasks;
+
+namespace GungeonMod.Items.Actives.PotionofGunFriendship
+{
+    public class GunBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Potion of Gun Friendship");
+            Description.SetDefault("+10% Attack Speed, +2 Knockback ");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+ 
+    
+    
+    
+    
+    
+    
+    }
+}
