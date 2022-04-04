e096cae40276ffcbe2a818a3ab72da394f062020
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-04-04 16:19:33 -0700

    Add files via upload

---
+++ Items/Accessories/Passives/BionicLeg/BionicLeg.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using GungeonMod;
+using System;
+namespace GungeonMod.Items.Accessories.Passives.BionicLeg
+{
+    public class BionicLeg : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bionic Leg");
+            Tooltip.SetDefault("+5% Movement Speed\n+5% Jump Speed\n+20 Max Life");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = 1;
+        }
+
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.statLifeMax2 += 20;
+            player.moveSpeed += 5;
+            player.jumpSpeedBoost += 5;
+        }
+
+    }
+}
---
+++ Items/Accessories/Passives/HeartSynthesizer/HeartSynthesizer.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using GungeonMod;
+using System;
+namespace GungeonMod.Items.Accessories.Passives.HeartSynthesizer
+{
+    public class HeartSynthesizer : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Heart Synthesizer");
+            Tooltip.SetDefault("Hitting enemies has a chance to spawn a heart");
+        }
+
+        public override void SetDefaults()
+        {
+            item.accessory = true;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = 1;
+        }
+
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<GungeonPlayer>().HeartSynthesizer = true;
+        }
+
+    }
+}
