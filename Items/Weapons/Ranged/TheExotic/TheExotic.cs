f155b0d370543b628e5076c10b2574f7f8036a50
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-04-04 16:18:58 -0700

    Add files via upload

---
+++ Items/Weapons/Ranged/TheExotic/TheExotic.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GungeonMod;
+using GungeonMod.Items.Herbs;
+namespace GungeonMod.Items.Weapons.Ranged.TheExotic
+{
+	public class TheExotic : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("The Exotic");
+			Tooltip.SetDefault("Pack of Wolves");
+		}
+		public override void SetDefaults()
+		{
+			// Stats of the item
+			item.damage = 120;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.knockBack = 6;
+			item.value = 1000;
+			item.ranged = true;
+			item.crit = 15;
+			// How the item works
+			item.autoReuse = true;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.noMelee = true;
+			//item.useAmmo = AmmoID.Bullet;
+			item.shoot = mod.ProjectileType("WolfPackRounds");
+			item.shootSpeed = 12.7f;
+			// Other
+			item.UseSound = mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/ExoticGunShot");
+			item.rare = 9;
+			item.scale = 1.3f;
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-22, 0);
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			//recipe.AddIngredient(ModContent.ItemType<PeaPod>(), 12);
+			recipe.AddIngredient(ItemID.FireworksLauncher,1);
+			recipe.AddIngredient(ItemID.SnowmanCannon, 1);
+			recipe.AddIngredient(ItemID.RocketLauncher, 1);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/Weapons/Ranged/TheExotic/WolfPackRounds.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+
+namespace GungeonMod.Items.Weapons.Ranged.TheExotic
+{
+  public class WolfPackRounds : ModProjectile
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Rocket 1");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.RocketI);
+			aiType = ProjectileID.RocketI;
+			projectile.hostile = false;
+			projectile.friendly = true;
+			projectile.ranged = true;
+		}
+        public override void Kill(int timeLeft)
+        {
+			if (projectile.ai[1] == 0)
+			{
+				for (int i = 0; i < 5; i++)
+				{
+					// Random upward vector.
+					Vector2 vel = new Vector2(Main.rand.NextFloat(-3, 3), Main.rand.NextFloat(-10, -8));
+					Projectile.NewProjectile(projectile.Center, vel, mod.ProjectileType("WolfPackRounds2"), projectile.damage, projectile.knockBack, projectile.owner, 0, 1);
+				
+				}
+			}
+		}
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+			
+            return base.OnTileCollide(oldVelocity);
+        }
+
+
+
+
+
+
+
+    }
+}
