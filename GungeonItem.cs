e6fb5244dfcb0a50ec79e2aa79f30b6d01b34bc6
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-04-13 17:42:33 -0700

    Add files via upload

--- GungeonItem.cs
+++ GungeonItem.cs
@@ -1,24 +1,48 @@
 ﻿
+using GungeonMod.Items.Accessories.BulletUpgrades.Plus1Bullets;
+using GungeonMod.Items.Weapons.Ranged.BlackHoleGun;
+using GungeonMod.Items.Accessories.Passives.BionicLeg;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
-using GungeonMod.Items.Herbs;
 using GungeonMod.Items.Actives.PotionofGunFriendship;
+
 namespace GungeonMod
 {
-   public  class GungeonItem : GlobalItem
+    public class GungeonItem : GlobalItem
     {
-		public override void OpenVanillaBag(string context, Player player, int arg)
-		{
-			if (context == "herbBag")
-			{
-				player.QuickSpawnItem(ItemType<PeaSeeds>(), Main.rand.Next(5, 13));
-			}
-			if (context == "bossBag" && arg == ItemID.WallOfFleshBossBag)
-			{
-				player.QuickSpawnItem(ItemType<PotionofGunFriendship>(), Main.rand.Next(8, 1));
-			}
-		}		
-	}
+        public override void OpenVanillaBag(string context, Player player, int arg)
+        {
+            float spawnChance = Main.rand.NextFloat();
+            switch (arg)
+            {
+                case ItemID.MoonLordBossBag:
+                    if (spawnChance < 0.67f)
+                    {
+                        player.QuickSpawnItem(player.GetItemSource_OpenItem(ItemID.MoonLordBossBag),ModContent.ItemType<BlackHoleGun>());
+                    }
+                    break;
+                case ItemID.GolemBossBag:
+                    if (spawnChance < 0.67f)
+                    {
+                        player.QuickSpawnItem(player.GetItemSource_OpenItem(ItemID.MoonLordBossBag), ModContent.ItemType<Plus1Bullets>());
+                    }
+                    break;
+                case ItemID.SkeletronPrimeBossBag:
+                    if (spawnChance < 0.67f)
+                    {
+                        player.QuickSpawnItem(player.GetItemSource_OpenItem(ItemID.MoonLordBossBag), ModContent.ItemType<BionicLeg>());
+                    }
+                    break;
+                case ItemID.WallOfFleshBossBag:
+                    if (spawnChance < 0.67f)
+                    {
+                        player.QuickSpawnItem(player.GetItemSource_OpenItem(ItemID.MoonLordBossBag), ModContent.ItemType<PotionofGunFriendship>());
+                    }
+                    break;
+            }
+        }
+    }
 }
