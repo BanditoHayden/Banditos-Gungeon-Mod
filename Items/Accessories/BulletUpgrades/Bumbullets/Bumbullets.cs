2b74dd1242c77f998529ae03563877d1f9f030ee
BanditoHayden <101084672+BanditoHayden@users.noreply.github.com>
2022-04-14 21:11:52 -0700

    Add files via upload

---
+++ Items/Accessories/BulletUpgrades/Bumbullets/Bumbullets.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using GungeonMod;
+using System;
+namespace GungeonMod.Items.Accessories.BulletUpgrades.Bumbullets
+{
+    public class Bumbullets : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bumbullets");
+            Tooltip.SetDefault("Bumblecore\nShooting occasionally spawns additional bees.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.value = Item.sellPrice(0, 1, 0, 0);
+            Item.rare = 2;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+            .AddIngredient(ItemID.MusketBall, 100)
+            .AddCondition(Recipe.Condition.NearLava)
+            .Register();
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<GungeonPlayer>().Bumble = true;
+        }
+    }
+}
